d866ce94742b9f25ac38568aa1c76dabd929c0fc
Pro100sasha <sasha_ablin@rambler.ru>
2026-03-26 11:26:03 +0500

    add auth-module

---
+++ packages/auth-module/src/AuthService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Auth;
+
+class AuthService
+{
+    public function checkStatus()
+    {
+        return "Модуль Auth успешно подключен и работает!";
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\AuthService;
 
 Route::get('/', function () {
-    return view('welcome');
+    $auth = new AuthService();
+    return $auth->checkStatus();
 });
